5920f6f566c8eb59dd65e92739f0bf74efb3c99d
yury <yu.moskalev@gmail.com>
2021-04-18 20:17:07 +0300

    fixed 6.1 task

--- 06_lesson/hw6_1.sql
+++ 06_lesson/hw6_1.sql
@@ -11,11 +11,13 @@
 SET  @selected_user_id :=  4;
 
 -- введем переменную для максимального числа сообщений нашему пользователю
-SET @max_cnt := (SELECT COUNT(*) FROM messages WHERE to_user_id = @selected_user_id GROUP BY from_user_id LIMIT 1);
+SET @max_cnt := (SELECT COUNT(*) AS cnt FROM messages WHERE to_user_id = @selected_user_id
+GROUP BY from_user_id ORDER BY cnt DESC LIMIT 1);
 
 -- выводим имя и фамилию пользователя/ей, написавшего/их наибольшее число сообщений, и само число сообщений
 SELECT concat(first_name, ' ', last_name) AS name,
-(SELECT COUNT(*) FROM messages WHERE to_user_id = @selected_user_id GROUP BY from_user_id LIMIT 1) AS cnt_msgs
+(SELECT COUNT(*) AS cnt2 FROM messages WHERE to_user_id = @selected_user_id
+GROUP BY from_user_id ORDER BY cnt2 DESC LIMIT 1) AS cnt_msgs
 FROM users
 WHERE id IN (SELECT from_user_id FROM messages WHERE to_user_id = @selected_user_id GROUP BY from_user_id
 						having  COUNT(*) = @max_cnt
